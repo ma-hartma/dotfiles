c9d39fa5495713b65390db2081fac73d01bba3fd
mahartma <mahartma@mahartma.com>
2020-08-12 20:42:24 +0200

    Adding Copyright

--- packages/arch/dev.sh
+++ packages/arch/dev.sh
@@ -32,10 +32,6 @@ PACKAGES=("${PACKAGES[@]}"
     "jdk" "jre"
     # REST tools
     "postman-bin"
-
-
-
-
 )
 
 # To be added:
--- setup_linux.sh
+++ setup_linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 #   Copyright (c) 2017-2019, Christian Muehlhaeuser <muesli@gmail.com>
+#   Copyright (c) 2020-2020, Matthias Hartmann <mahartma@mahartma.com>
 #
 #     For license see LICENSE
 #
